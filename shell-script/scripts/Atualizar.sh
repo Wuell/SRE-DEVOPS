ef1d8e9e01b3b807987310cdef2a97d3d7b64c15
Wuell <wellerson.veloso10@gmail.com>
2025-10-16 10:40:54 -0300

    Estruturando mais o script Atualizar.sh

--- shell-script/scripts/Atualizar.sh
+++ shell-script/scripts/Atualizar.sh
@@ -4,5 +4,9 @@ DIR="$HOME/SRE-DEVOPS/shell-script/scripts"
 
 sudo apt-get update
 sudo apt-get upgrade
-export PATH=$PATH:"$DIR"
-echo "Diretorio $DIR adicionado as variaveis de ambiente"
+export PATH="$PATH:$DIR"
+if echo "$PATH" | grep -q "$DIR"; then
+	echo "Diretorio $DIR adicionado as variaveis de ambiente"
+else
+	echo "Diretorio $DIR nao foi adicionado as variaveis de ambiente"
+fi
