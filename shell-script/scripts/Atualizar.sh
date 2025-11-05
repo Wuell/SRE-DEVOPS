968dd16050f600a520dfa31313a93f7683f39184
Wuell <Wellerson.veloso10@gmail.com>
2025-11-05 08:19:05 -0300

    Adicionando git-tasks ao meu repositorio de atividades

--- shell-script/scripts/Atualizar.sh
+++ shell-script/scripts/Atualizar.sh
@@ -2,11 +2,5 @@
 
 DIR="$HOME/SRE-DEVOPS/shell-script/scripts"
 
-sudo apt-get update
-sudo apt-get upgrade
-export PATH="$PATH:$DIR"
-if echo "$PATH" | grep -q "$DIR"; then
-	echo "Diretorio $DIR adicionado as variaveis de ambiente"
-else
-	echo "Diretorio $DIR nao foi adicionado as variaveis de ambiente"
-fi
+sudo apt-get update -y
+sudo apt-get upgrade -y
